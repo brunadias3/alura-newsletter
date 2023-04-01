cb1fa77ecd22341024b31f49a839fdfcc807fa79
Bruna <brunadias_30@hotmail.com>
2023-04-01 18:16:55 -0300

    setting dark theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { MoonIcon, } from '@heroicons/react/24/solid'
+import ToggleTheme from '../ToggleTheme/ToggleTheme'
 
 export default function Header() {
   return (
     <div className='flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5'>
         <span className='text-gray-100'>Olá, Usuário</span>
         <h1 className='text-gray-100 text-xl'>Alura Newsletter</h1>
-        <MoonIcon className='h-8 text-gray-100 cursor-pointer hidden sm:block' />
+        <ToggleTheme />
     </div>
   )
 }
