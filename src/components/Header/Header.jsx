60bdcac8b8ed7c724ff6c7afb15db8ec22fccc36
Bruna <brunadias_30@hotmail.com>
2023-04-01 20:09:34 -0300

    creating form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ToggleTheme from '../ToggleTheme/ToggleTheme'
 
-export default function Header() {
+export default function Header({ user }) {
   return (
     <div className='flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5'>
-        <span className='text-gray-100'>Olá, Usuário</span>
-        <h1 className='text-gray-100 text-xl'>Alura Newsletter</h1>
-        <ToggleTheme />
+      <span className='text-gray-100'>Olá, {user?.name || 'Usuário'}</span>
+      <h1 className='text-gray-100 text-xl'>Alura Newsletter</h1>
+      <ToggleTheme />
     </div>
   )
 }
