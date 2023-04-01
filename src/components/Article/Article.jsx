74f9f1252864faa25cfda08bb5e143a81b053eb7
Bruna <brunadias_30@hotmail.com>
2023-04-01 19:28:55 -0300

    styling article

---
+++ src/components/Article/Article.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Article({ title, text, tags, image, alt }) {
+    return (
+        <div className='p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-xl flex flex-col items-center gap-2'>
+            <h1 className=' mb-2 text-xl text-alura-200 dark:text-gray-200 font-bold'>{title}</h1>
+            <div className='flex flex-col gap-1'>
+                {
+                    text.map((content, index) => <span key={index} className='text-alura-200 dark:text-gray-400'>{content}</span>)
+                }
+            </div>
+        </div>
+    )
+}
--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -1,11 +1,12 @@
 import data from "../../../data.json"
+import Article from "../Article/Article"
 
 export default function ArticleList() {
   return (
-    <div className="mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3">
-        {
-            data.map(props => <p>{props.title}</p>)
-        }
+    <div className="mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3 lg:max-w-[1000px]">
+      {
+        data.map((article, index) => <Article key={index} {...article} />)
+      }
     </div>
   )
 }
