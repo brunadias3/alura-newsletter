37a40bc931933fc7c9ac037c29309303df15e44d
Bruna <brunadias_30@hotmail.com>
2023-04-19 09:45:09 -0300

    melhorando legibilidade

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,7 +15,7 @@ export default function Form({ onSubmit }) {
         <form onSubmit={safeSubmit} className='h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5'>
             <Input type='text' required placeholder='Insira seu nome...' />
             <Input type='email' required placeholder='Insira seu e-mail...' />
-            <button type='submit' className='py-1 px-5 w-full max-w-sm bg-alura-100 dark:bg-alura-200 rounded-full text-gray-200 outline-none uppercase hover:animate-pulse hover:shadow-md hover:shadow-gray-500 hover:dark:shadow-black'>Seguir</button>
+            <button type='submit' className='alura-button'>Seguir</button>
         </form>
     )
 }
