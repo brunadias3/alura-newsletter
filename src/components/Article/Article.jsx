b0edcf5061ea3ff9690c86a023ee40d2dfc81f52
Bruna <brunadias_30@hotmail.com>
2023-04-19 09:19:42 -0300

    pseudo elementos

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function Article({ title, text, tags, image, alt }) {
     return (
-        <div className='p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-xl flex flex-col items-center gap-2'>
+        <div className='p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-xl flex flex-col items-center gap-2 sm:hover:shadow-gray-500 sm:hover:shadow-lg sm:hover:dark:shadow-gray-500 sm:hover:dark:shadow-lg'>
             <h1 className=' mb-2 text-xl text-alura-200 dark:text-gray-200 font-bold'>{title}</h1>
             <div className='w-full flex-row justify-end gap-2 pr-5 sm:flex'>
                 {
                     tags.map((tag) => 
                         <span 
                             key={tag}
-                            className='bg-alura-100 dark:bg-dark-100 px-4 py-2 rounded-full text-gray-200 text-xs font-bold uppercase'
+                            className='bg-alura-100 dark:bg-dark-100 px-4 py-2 rounded-full text-gray-200 text-xs font-bold uppercase hover:bg-alura-200 hover:scale-110'
                             >
                                 {tag}
                         </span>)
